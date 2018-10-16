42db65d691b9be0ff9661bc3ad33a9aa4f962200
kwanwooi25 <kwanwoo.jeong@gmail.com>
2018-10-16 16:26:59 +0900

    Updates

---
+++ precache-manifest.2560fa282513c9736260690168c9118d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "303d460b57c14e7b062a847ff692c487",
+    "url": "/js-playground/static/media/kw-logo.303d460b.svg"
+  },
+  {
+    "revision": "007b131b7db1368d1af9",
+    "url": "/js-playground/static/js/runtime~main.007b131b.js"
+  },
+  {
+    "revision": "193783fc131aa8c90f2f",
+    "url": "/js-playground/static/js/main.193783fc.chunk.js"
+  },
+  {
+    "revision": "106510931b71ef51140e",
+    "url": "/js-playground/static/js/1.10651093.chunk.js"
+  },
+  {
+    "revision": "193783fc131aa8c90f2f",
+    "url": "/js-playground/static/css/main.f23c60bc.chunk.css"
+  },
+  {
+    "revision": "106510931b71ef51140e",
+    "url": "/js-playground/static/css/1.be634000.chunk.css"
+  },
+  {
+    "revision": "c9a42a03d9842107701a4e628481b05b",
+    "url": "/js-playground/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/js-playground/precache-manifest.2560fa282513c9736260690168c9118d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/js-playground/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
